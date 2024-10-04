08ce5d9cccc69e6fbd6d776bd4d32a28080985e3
Morais <vitormorais52@gmail.com>
2024-10-04 00:11:52 -0300

    feat: creates utils folder with general functions file and adds a function for grouping data

---
+++ src/utils/functions.ts
@@ -0,0 +1,27 @@
+import { Registration } from "@/models/registration";
+
+interface GroupedRegistrations {
+  review: Registration[];
+  approved: Registration[];
+  rejected: Registration[];
+}
+
+export const groupByStatus = (
+  registrations: Registration[]
+): GroupedRegistrations => {
+  const initialValue = { review: [], approved: [], rejected: [] };
+
+  if (!registrations) return initialValue;
+
+  const groupedRegistrations = registrations.reduce<GroupedRegistrations>(
+    (accumulator, registration) => {
+      const status = registration.status as keyof GroupedRegistrations;
+      if (status in accumulator) {
+        accumulator[status].push(registration);
+      }
+      return accumulator;
+    },
+    initialValue
+  );
+  return groupedRegistrations;
+};
