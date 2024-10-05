d9446cdb32b92d9374bdfdad452dccb4cc17a093
Morais <vitormorais52@gmail.com>
2024-10-05 01:17:05 -0300

    feat: adds functionality to the refresh button

--- src/pages/Dashboard/components/Searchbar/index.tsx
+++ src/pages/Dashboard/components/Searchbar/index.tsx
@@ -1,22 +1,33 @@
-import { HiRefresh } from "react-icons/hi";
 import { useHistory } from "react-router-dom";
+import { useQueryClient } from "@tanstack/react-query";
+
+import routes from "@/router/routes";
+
 import Button from "@/components/Buttons";
 import { IconButton } from "@/components/Buttons/IconButton";
 import TextField from "@/components/TextField";
-import routes from "@/router/routes";
+
+import { HiRefresh } from "react-icons/hi";
 import * as S from "./styles";
+
 export const SearchBar = () => {
   const history = useHistory();
+  const queryClient = useQueryClient();
 
   const goToNewAdmissionPage = () => {
     history.push(routes.newUser);
   };
 
+  const refecthRegistrations = async () =>
+    await queryClient.refetchQueries({
+      queryKey: ["registrations"],
+    });
+
   return (
     <S.Container>
       <TextField placeholder="Digite um CPF válido" />
       <S.Actions>
-        <IconButton aria-label="refetch">
+        <IconButton aria-label="refetch" onClick={() => refecthRegistrations()}>
           <HiRefresh />
         </IconButton>
         <Button onClick={() => goToNewAdmissionPage()}>Nova Admissão</Button>
